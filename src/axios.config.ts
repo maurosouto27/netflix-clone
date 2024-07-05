f1fb6fab1b6c60beba2842c130cddf4a44e25b11
mauro-souto <msouto@xseed.com.uy>
2024-07-05 11:17:55 -0300

    proxy removed for testing

--- src/axios.config.ts
+++ src/axios.config.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  // baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    Accept: "application/json",
+    Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+  },
 });
 
 export default axiosInstance;
--- vite.config.ts
+++ vite.config.ts
@@ -8,29 +8,29 @@ export default ({ mode }: { mode: string }) => {
 
   return defineConfig({
     plugins: [react()],
-    server: {
-      proxy: {
-        "/.well-known/acme-challenge/": {
-          target: "https://netflix-clone-alpha-black.vercel.app", // O el puerto que estés usando localmente
-          bypass: (req) => {
-            return req.url;
-          },
-        },
-        // Configuración existente para la API de TMDB
-        "/api": {
-          target: process.env.VITE_API_BASE_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-          configure: (proxy) => {
-            proxy.on("proxyReq", (proxyReq) => {
-              proxyReq.setHeader(
-                "Authorization",
-                `Bearer ${process.env.VITE_ACCESS_TOKEN}`
-              );
-            });
-          },
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     "/.well-known/acme-challenge/": {
+    //       target: "https://netflix-clone-alpha-black.vercel.app", // O el puerto que estés usando localmente
+    //       bypass: (req) => {
+    //         return req.url;
+    //       },
+    //     },
+    //     // Configuración existente para la API de TMDB
+    //     "/api": {
+    //       target: process.env.VITE_API_BASE_URL,
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/api/, ""),
+    //       configure: (proxy) => {
+    //         proxy.on("proxyReq", (proxyReq) => {
+    //           proxyReq.setHeader(
+    //             "Authorization",
+    //             `Bearer ${process.env.VITE_ACCESS_TOKEN}`
+    //           );
+    //         });
+    //       },
+    //     },
+    //   },
+    // },
   });
 };
